7474af06a69696831b0273af6b1ea3125fa4ef43
Alex Firsov <aragorn.strider.dunadan@gmail.com>
2023-01-06 20:23:51 +0200

    Change input to files

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ The HTML result
 ```yaml
 - name: Convert markdown text to html
   id: md-to-html
-  uses: Synqrinus/action-md-to-html@v1.1
+  uses: Synqrinus/action-md-to-html@v1.3
   with:
     text: '# Hello there'
 
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const core = require('@actions/core');
 const showdown = require('showdown');
+const fs = require('fs');
 
 try {
-  const markdownText = core.getInput('text');
-  const converter = new showdown.Converter();
-  const html = converter.makeHtml(markdownText);
-
-  core.setOutput("html", html);
-
+    const markdownText = fs.readFileSync(core.getInput('text'), 'utf8').toString().trim()
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(markdownText);
+    fs.writeFileSync(core.getInput('html'), html, {encoding: "utf8", mode: "w"})
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
