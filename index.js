34f5504b8c4b5d78b44e1a2c3bc9688f50c58b41
Alex Firsov <aragorn.strider.dunadan@gmail.com>
2023-01-06 20:27:50 +0200

    Change input to files fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
     const markdownText = fs.readFileSync(core.getInput('text'), 'utf8').toString().trim()
     const converter = new showdown.Converter();
     const html = converter.makeHtml(markdownText);
-    fs.writeFileSync(core.getInput('html'), html, {encoding: "utf8", mode: "w"})
+    fs.writeFileSync(core.getInput('html'), html, {encoding: "utf8", flag: "w"})
 } catch (error) {
     core.setFailed(error.message);
 }
